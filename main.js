571c720c1b620a4b27869b77daccee908c98e28e
Ashley Almonte <ashleyalmonte14@gmail.com>
2019-05-01 13:59:32 -0400

    added the first part yes

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
+var prices = [33, 26, 99, 120, 12, 45];
+var sum= 0;
+var count;
+for (count = 0; count < prices.length; count++) {
+  sum = pricess[count] + sum;
+}
+console.log('your sum is' + sum);
+}
+
+
+
 for(crowNum = 99; crowNum >= 1; crowNum--){
   if(crowNum != 1) {
   console.log(crowNum + "crows on the wall" + crowNum + "crows." + "1 fell down and became wight" + (crowNum - 1) + "crows are left."
  } else {
   console.log("1 single crow. It fell down and became a wight. There is no one left to defend Westeros now.");
+}
 
-  }
+let count = 99
+while (count < 98) {
+  if () {
+console.log('99');
